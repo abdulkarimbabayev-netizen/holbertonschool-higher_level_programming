5e9a09a13f1a80332067575fea55cc7fa9c1d9d6
Abdulkarim Babayev <13171@holbertonstudents.com>
2026-03-14 16:33:18 +0000

    Add 6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for j in range(len(row)):
+            print("{:d}".format(row[j]), end=" " if j != len(row) - 1 else "")
+        print("")
