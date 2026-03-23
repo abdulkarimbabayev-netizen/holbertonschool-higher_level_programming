b51c6b060eb114a23d6f23a16e4ab978101b7efd
Abdulkarim Babayev <13171@holbertonstudents.com>
2026-03-23 19:15:25 +0000

    Remove python-classes directory

--- python-classes/1-square.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-class Square:
-    """Square class with a private size attribute."""
-
-    def __init__(self, size):
-        self.__size = size
---
+++ python-more_data_structures/12-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+
+print("X = {}".format(roman_to_int("X")))
+print("VII = {}".format(roman_to_int("VII")))
+print("IX = {}".format(roman_to_int("IX")))
+print("LXXXVII = {}".format(roman_to_int("LXXXVII")))
+print("DCCVII = {}".format(roman_to_int("DCCVII")))
