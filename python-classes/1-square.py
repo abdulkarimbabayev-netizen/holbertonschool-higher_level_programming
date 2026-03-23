daf58df233efbb72179245432dbcd869af677ea9
Abdulkarim Babayev <13171@holbertonstudents.com>
2026-03-23 18:52:48 +0000

    Add tasks 1, 2, and 3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
 class Square:
-    """Square class with a private size attribute."""
+    """Represents a square."""
 
     def __init__(self, size):
+        """Initialize the square.
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
