fcbae59267b820b3f319b3d98c7adfc092a7a32b
Abdulkarim Babayev <13171@holbertonstudents.com>
2026-03-25 15:35:59 +0000

    6. Coordinates of a square - Fixed validation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -64,11 +64,13 @@ class Square:
         Raises:
             TypeError: If value is not a tuple of 2 positive integers.
         """
-        if (not isinstance(value, tuple) or len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if isinstance(value, tuple) and len(value) == 2:
+            if all(isinstance(num, int) and num >= 0 for num in value):
+                self.__position = value
+            else:
+                raise TypeError("position must be a tuple of 2 positive integers")
+        else:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """Return the current square area.
