9da9b0a176bf6fab9905c7cb699d5a0facb56442
Abdulkarim Babayev <13171@holbertonstudents.com>
2026-04-04 06:52:37 +0000

    Fix task 2 - fix line too long pycodestyle error

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,7 +8,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, headers={'cfclearance': 'true'})
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data=data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
